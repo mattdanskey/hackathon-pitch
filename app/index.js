fea40a0e86c41d2143903488e2c31383ac945c0e
Matt Danskey (InComm) <mdanskey@gmail.com>
2015-12-09 14:29:59 -0800

    push for troubleshooting es6 issue

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 "use strict";
 var component = require('./component');
 var app = document.createElement('div');
+require('./store.js')
 
-console.log("yea woot yknow")
 
 document.body.appendChild(app);
 
@@ -11,13 +11,5 @@ let counter = 0;
 if(module.hot){
   module.hot.accept('./component', function(){
     let newApp = require('./component')()
-    //Why does the below line do nothing?
-    document.body.lastChild.innerHTML = newApp.innerHTML
-    console.log('ACCEPT MY THANKS', counter)
-    counter++
   });
-
-  // module.hot.dispose(function(){
-  //
-  // });
 }
