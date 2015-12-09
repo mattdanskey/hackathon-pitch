f6c94d3f7355aedd66299607874d5947f5304c8b
Matt Danskey (InComm) <mdanskey@gmail.com>
2015-12-09 14:16:27 -0800

    commit one of the mysteries of the universe

--- app/component.js
+++ app/component.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   var element = document.createElement('h1');
 
-  element.innerHTML = 'Helloooeoeejejdididde';
+  element.innerHTML = 'Hi';
 
   return element;
 };
--- app/index.js
+++ app/index.js
@@ -1,18 +1,23 @@
+"use strict";
 var component = require('./component');
 var app = document.createElement('div');
 
-console.log("yea woot")
+console.log("yea woot yknow")
 
 document.body.appendChild(app);
 
 app.appendChild(component());
-
+let counter = 0;
 if(module.hot){
-  module.hot.accept();
+  module.hot.accept('./component', function(){
+    let newApp = require('./component')()
+    //Why does the below line do nothing?
+    document.body.lastChild.innerHTML = newApp.innerHTML
+    console.log('ACCEPT MY THANKS', counter)
+    counter++
+  });
 
-  module.hot.dispose(function(){
-    while(document.body.hasChildNodes()) {
-      document.body.removeChild(document.body.lastChild);
-    }
-  })
+  // module.hot.dispose(function(){
+  //
+  // });
 }
