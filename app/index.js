bc0db817ecd78d4487f4ec19723583bd94342f78
Matt Danskey (InComm) <mdanskey@gmail.com>
2015-12-09 13:06:04 -0800

    playing with hot modules

--- app/index.js
+++ app/index.js
@@ -1,6 +1,18 @@
 var component = require('./component');
 var app = document.createElement('div');
 
+console.log("yea woot")
+
 document.body.appendChild(app);
 
 app.appendChild(component());
+
+if(module.hot){
+  module.hot.accept();
+
+  module.hot.dispose(function(){
+    while(document.body.hasChildNodes()) {
+      document.body.removeChild(document.body.lastChild);
+    }
+  })
+}
